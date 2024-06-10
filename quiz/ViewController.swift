704e09be018b2a3cafcf11048773ac5fde2b3828
anika <Anna.Fedchenko2002@yandex.ru>
2024-06-10 21:49:22 +0300

    created

---
+++ quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  quiz
+//
+//  Created by Anna Fedchenko on 10.06.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
